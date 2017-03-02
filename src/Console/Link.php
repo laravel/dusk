24256ca4a161d98bc2507e60c16c93da317d4ad7
Xander Smalbil <xander@netbulae.eu>
2017-03-02 21:43:04 +0100

    Command to create a symbolic link to the screenshots dir

---
+++ src/Console/Link.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Laravel\Dusk\Console;
+
+use Illuminate\Console\Command;
+
+class Link extends Command
+{
+    /**
+     * The console command signature.
+     *
+     * @var string
+     */
+    protected $signature = 'dusk:link';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a symbolic link from "tests/Browser/screenshots" to "storage/app/public/screenshots"';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function fire()
+    {
+        if (file_exists(storage_path('app/public/screenshots'))) {
+            return $this->error('The "app/public/screenshots" directory already exists.');
+        }
+
+        $this->laravel->make('files')->link(
+            base_path('tests/Browser/screenshots'), storage_path('app/public/screenshots')
+        );
+
+        $this->info('The [app/public/screenshots] directory has been linked.');
+    }
+}
