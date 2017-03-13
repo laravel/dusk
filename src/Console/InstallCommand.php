3de7058145233e1115d70d813d85ae7d5f35f1d4
Oussama Elgoumri <ktsnepyg9igfz1@gmail.com>
2017-03-13 23:50:03 +0000

    install the right chromedriver binary for the user system when running 'php artisan dusk:install'

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -37,6 +37,158 @@ class InstallCommand extends Command
      */
     public function handle()
     {
+        $this->downloadChromeDriver();
+        $this->createTestDirectories();
+        $this->copyStubs();
+    }
+
+    /**
+     * Download chromedriver binary.
+     *
+     * @return void
+     */
+    protected function downloadChromeDriver()
+    {
+        $this->info('Downloading chrome driver..');
+        $file = $this->getChromeDriverFileToDownload();
+        $filepath = $this->downloadToBinFolder($file);
+        $filepath = $this->unzip($filepath);
+        $this->setPermissions($filepath); 
+    }
+
+    /**
+     * Set the right permissions for the downloaded file.
+     *
+     * @param  string   $filepath
+     * @return void
+     */
+    private function setPermissions($filepath)
+    {
+        if (PHP_OS != 'WINNT') {
+            chmod($filepath, 0755);
+        }
+
+        return $filepath;
+    }
+
+    /**
+     * Unzip the given archive.
+     *
+     * @param  string    $filepath
+     * @throws \Exception
+     * @return string
+     */
+    private function unzip($filepath)
+    {
+        $fileinfo = pathinfo($filepath);
+        $zip = new \ZipArchive;
+
+        if ($zip->open($filepath)) {
+            $zip->extractTo($fileinfo['dirname']);
+            $zip->close();
+            unlink($filepath);
+            $filename = preg_replace('/(32|64)/', '', $fileinfo['filename']);
+            $filepath = $fileinfo['dirname'].DIRECTORY_SEPARATOR.str_replace('_', '-', $filename);
+            rename($fileinfo['dirname'].DIRECTORY_SEPARATOR.'chromedriver', $filepath);
+
+            return $filepath;
+        }
+
+        throw new \Exception("Could not extract {$filepath}");
+    }
+
+    /**
+     * Download file to bin folder.
+     *
+     * @param  string    $file
+     * @return string
+     */
+    private function downloadToBinFolder($file)
+    {
+        $pathinfo = pathinfo($file);
+        $filename = $pathinfo['basename'];
+        $filepath = realpath(__DIR__ . "/../../bin").DIRECTORY_SEPARATOR.$filename;
+
+        file_put_contents($filepath, fopen($file, 'r'));
+
+        return $filepath;
+    }
+
+    /**
+     * Get chrome driver file to download.
+     *
+     * @return string
+     */
+    private function getChromeDriverFileToDownload()
+    {
+        $latest_release = trim(file_get_contents('https://chromedriver.storage.googleapis.com/LATEST_RELEASE'));
+        $file = "https://chromedriver.storage.googleapis.com/{$latest_release}/chromedriver_%s.zip";
+        $architect = $this->getArchitect();
+        $file = sprintf($file, $architect);
+
+        $this->getDownloadableChromeDriverFile($file);
+        return $file;
+    }
+
+    /**
+     * @TODO: maybe it should die and throw exception if chrome driver file was not found.
+     *
+     * Get downloadable chrome driver file.
+     *
+     * @param string    $file
+     * @param bool      $rec
+     *
+     * @return string
+     */
+    private function getDownloadableChromeDriverFile($file, $rec = true)
+    {
+        $headers = @get_headers($file);
+
+        // Chrome driver not found for current system:
+        if (strpos($headers[0], '200 OK') === false) {
+            if (strpos($file, '32.zip') !== false) {
+                $file = str_replace('32.zip', '64.zip', $file);
+            } else {
+                $file = str_replace('64.zip', '32.zip', $file);
+            }
+
+            if ($rec) {
+                return $this->getDownloadableChromeDriverFile($file, false);
+            } else {
+                throw new \Exception("chromedriver binaries not found: {$file} for your system: " . $this->getArchitect());
+            }
+        }
+
+        return $file;
+    }
+
+    /**
+     * Get system architecture.
+     *
+     * @return  string
+     */
+    private function getArchitect()
+    {
+        $bit = PHP_INT_SIZE * 8;
+
+        if (PHP_OS == 'Darwin') {
+            return "mac{$bit}";
+        }
+
+        if (PHP_OS == 'WINNT') {
+            return "win{$bit}";
+        }
+
+        return "linux{$bit}";
+    }
+
+    /**
+     * Create test directories.
+     *
+     * @return  void
+     */
+    protected function createTestDirectories()
+    {
         $dirs = [
             'Pages' => false,
             'screenshots' => true,
@@ -45,10 +197,22 @@ class InstallCommand extends Command
 
         foreach ($dirs as $dir => $has_dot_gitignore) {
             if (!is_dir(base_path("tests/Browser/{$dir}"))) {
-                $this->createDirectory($dir, $has_dot_gitignore);
+                mkdir(base_path("tests/Browser/{$dir}"), 0755, true);
+
+                if ($has_dot_gitignore) {
+                    file_put_contents(base_path("tests/Browser/{$dir}/.gitignore"), '*'.PHP_EOL.'!.gitignore'.PHP_EOL);
+                }
             }
         }
+    }
 
+    /**
+     * Copy stubs to test directories.
+     *
+     * @return  void
+     */
+    protected function copyStubs()
+    {
         $subs = [
             'ExampleTest.stub' => base_path('tests/Browser/ExampleTest.php'),
             'HomePage.stub' => base_path('tests/Browser/Pages/HomePage.php'),
@@ -63,20 +227,5 @@ class InstallCommand extends Command
         }
 
         $this->info('Dusk scaffolding installed successfully.');
-    }
-
-    /**
-     * Create directory inside tests/Browser.
-     *
-     * @param  string  $dir
-     * @param  boolean  $has_dot_gitignore
-     */
-    protected function createDirectory($dir, $has_dot_gitignore)
-    {
-        mkdir(base_path("tests/Browser/{$dir}"), 0755, true);
-
-        if ($has_dot_gitignore) {
-            file_put_contents(base_path("tests/Browser/{$dir}/.gitignore"), '*'.PHP_EOL.'!.gitignore'.PHP_EOL);
-        }
     }
 }
--- tests/InstallCommandTest.php
+++ tests/InstallCommandTest.php
@@ -6,17 +6,84 @@ class InstallCommandTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
     {
-        rename(base_path('tests'), base_path('tests-bkp'));
         Mockery::mock(Illuminate\Console\Command::class);
-    }
 
-    public function test_workflow()
-    {
-        $ic = Mockery::mock(InstallCommand::class)
+        $this->ic = Mockery::mock(InstallCommand::class)
             ->makePartial()
             ->shouldReceive('info')
             ->mock();
-        $ic->handle();
+    }
+
+    public function test_downloadChromeDriver()
+    {
+        $this->invokeMethod('downloadChromeDriver');
+    }
+
+    public function test_unzip_setPermissions()
+    {
+        $file = $this->invokeMethod('getChromeDriverFileToDownload');
+        $filepath = $this->invokeMethod('downloadToBinFolder', $file);
+        $new_filepath = $this->invokeMethod('unzip', $filepath);
+        $this->invokeMethod('setPermissions', $new_filepath);
+
+        $this->assertFileNotExists($filepath);
+        $this->assertFileExists($new_filepath);
+        $this->assertEquals('0755', substr(sprintf('%o', fileperms($new_filepath)), -4));
+        unlink($new_filepath);
+    }
+
+    public function test_downloadToBinFolder()
+    {
+        $file = $this->invokeMethod('getChromeDriverFileToDownload');
+        $filepath = $this->invokeMethod('downloadToBinFolder', $file);
+        
+        $this->assertFileExists($filepath);
+        $this->assertTrue(filesize($filepath) > 0);     // at least 1 byte :)
+        unlink($filepath);
+    }
+
+    public function test_getDownloadableChromeDriverFile()
+    {
+        $file = $this->invokeMethod(
+            'getDownloadableChromeDriverFile', 
+            'https://chromedriver.storage.googleapis.com/2.28/chromedriver_mac32.zip'
+        );
+
+        $this->assertEquals($file, 'https://chromedriver.storage.googleapis.com/2.28/chromedriver_mac64.zip');
+
+        try {
+            $this->invokeMethod(
+                'getDownloadableChromeDriverFile', 
+                'https://chromedriver.storage.googleapis.com/2.28/chromedriver_macFAKE.zip'
+            );
+
+            $this->assertTrue(false);
+        } catch (\Exception $e) {
+            $this->assertTrue(true);
+        }
+    }
+
+    public function test_getChromeDriverFileToDownload()
+    {
+        $file = $this->invokeMethod('getChromeDriverFileToDownload');
+        $this->assertRegExp(
+            '/https:\/\/chromedriver.storage.googleapis.com\/\d\.\d\d\/chromedriver_(linux|mac|win)(32|64).zip/',
+            $file
+        );
+    }
+
+    public function test_getArchitect()
+    {
+        $architect = $this->invokeMethod('getArchitect');
+        $this->assertRegExp('/(linux|mac|win)(32|64)/', $architect);
+    } 
+
+    public function test_createTestDirectories_copyStubs()
+    {
+        $this->backupTestDir();
+
+        $this->invokeMethod('createTestDirectories');
+        $this->invokeMethod('copyStubs');
 
         $common_dir = base_path('tests/Browser');
         $should_exists = [
@@ -35,9 +102,35 @@ class InstallCommandTest extends PHPUnit_Framework_TestCase
 
         // and the hunging fruit
         $this->assertFileExists(base_path('tests/DuskTestCase.php'));
+
+        $this->restoreTestDir();
     }
 
-    public function tearDown()
+    /**
+     * Invoke inaccessible methods.
+     *
+     * @param  string    $method
+     * @return mixed
+     */
+    private function invokeMethod($method, ...$args)
+    {
+        $m = new ReflectionMethod(InstallCommand::class, $method);
+        $m->setAccessible(true);
+        return $m->invokeArgs($this->ic, $args);
+    }
+
+    /**
+     * Backup test directory.
+     */
+    private function backupTestDir()
+    {
+        rename(base_path('tests'), base_path('tests-bkp'));
+    }
+
+    /**
+     * Restore test directory.
+     */
+    private function restoreTestDir()
     {
         $this->rmdir(base_path('tests'));
         rename(base_path('tests-bkp'), base_path('tests'));
@@ -69,4 +162,3 @@ class InstallCommandTest extends PHPUnit_Framework_TestCase
         rmdir($path); 
     }
 }
-
