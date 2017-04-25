299dfe0b21d4b060d01a4bddebef2e474277e8b8
Nick Hughes <nick.hughes@multiply.com>
2017-04-25 12:20:00 -0500

    added assertPathBeginsWith assertion

--- src/Concerns/MakesAssertions.php
+++ src/Concerns/MakesAssertions.php
@@ -52,6 +52,21 @@ trait MakesAssertions
     }
 
     /**
+     * Assert that the current URL path begins with given path.
+     *
+     * @param  string  $path
+     * @return $this
+     */
+    public function assertPathBeginsWith($path)
+    {
+        PHPUnit::assertStringStartsWith($path, parse_url(
+            $this->driver->getCurrentURL()
+        )['path']);
+
+        return $this;
+    }
+
+    /**
      * Assert that the current URL path does not match the given path.
      *
      * @param  string  $path
