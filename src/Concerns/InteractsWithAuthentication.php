2524d8800ecb021ab9dafb1316ae9b9490b4c077
Gabriel Caruso <carusogabriel34@gmail.com>
2018-01-06 23:19:24 -0200

    [2.0] Missing type hinting

--- src/Concerns/InteractsWithAuthentication.php
+++ src/Concerns/InteractsWithAuthentication.php
@@ -86,7 +86,7 @@ trait InteractsWithAuthentication
     /**
      * Assert that the user is authenticated as the given user.
      *
-     * @param  $user
+     * @param  dynamic $user
      * @param  string|null  $guard
      * @return $this
      */
