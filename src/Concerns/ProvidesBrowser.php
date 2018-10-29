afd90a6dc900c2f2da21324cd17a370492765cbb
coden <admitrjuk@findevelop.ru>
2018-10-29 15:52:17 +0700

    stash

--- src/Concerns/ProvidesBrowser.php
+++ src/Concerns/ProvidesBrowser.php
@@ -92,7 +92,6 @@ trait ProvidesBrowser
         for ($i = count(static::$browsers); $i < $this->browsersNeededFor($callback); $i++) {
             static::$browsers[] = $this->newBrowser($this->createWebDriver());
         }
-
         return static::$browsers;
     }
 
