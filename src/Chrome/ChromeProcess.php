ac1f0f5d49f3df0148dc21dde9d538f88ef4e49a
Michel Bardelmeijer <m.bardelmeijer@gmail.com>
2019-02-24 17:55:11 +0100

    Ensure the DISPLAY variable is set when the env variable is empty

--- src/Chrome/ChromeProcess.php
+++ src/Chrome/ChromeProcess.php
@@ -79,7 +79,7 @@ class ChromeProcess
             return [];
         }
 
-        return ['DISPLAY' => env('DISPLAY', ':0')];
+        return ['DISPLAY' => getenv('DISPLAY') ?: ':0'];
     }
 
     /**
