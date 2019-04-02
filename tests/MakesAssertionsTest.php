laravel/dusk

commit 1985616f82b702f29f9a302657e6bf3f65c5e8de

Avoid deprecation messages on PHPUnit 8

src/Concerns/MakesAssertions.php
@@ -205,8 +205,8 @@ trait MakesAssertions
      */
     public function assertSourceHas($code)
     {
-        PHPUnit::assertContains(
-            $code, $this->driver->getPageSource(),
+        PHPUnit::assertTrue(
+            Str::contains($this->driver->getPageSource(), $code),
             "Did not find expected source code [{$code}]"
         );
 
@@ -221,8 +221,8 @@ trait MakesAssertions
      */
     public function assertSourceMissing($code)
     {
-        PHPUnit::assertNotContains(
-            $code, $this->driver->getPageSource(),
+        PHPUnit::assertFalse(
+            Str::contains($this->driver->getPageSource(), $code),
             "Found unexpected source code [{$code}]"
         );
 
@@ -737,7 +737,9 @@ JS;
      */
     public function assertVueContains($key, $value, $componentSelector = null)
     {
-        PHPUnit::assertContains($value, $this->vueAttribute($componentSelector, $key));
+        PHPUnit::assertTrue(
+            Str::contains($this->vueAttribute($componentSelector, $key), $value)
+        );
 
         return $this;
     }
@@ -753,7 +755,9 @@ JS;
      */
     public function assertVueDoesNotContain($key, $value, $componentSelector = null)
     {
-        PHPUnit::assertNotContains($value, $this->vueAttribute($componentSelector, $key));
+        PHPUnit::assertFalse(
+            Str::contains($this->vueAttribute($componentSelector, $key), $value)
+        );
 
         return $this;
     }

commit 6a6c64fd6544f8c27ad2b7533a27d34ede3b8436

Fix assertVueContains
This fixes the assertVueContains and assertVueDoesNotContain methods to properly check for an array and what it contains instead of checking for a string. This got broken in https://github.com/laravel/dusk/commit/1985616f82b702f29f9a302657e6bf3f65c5e8de

Fixes https://github.com/laravel/dusk/issues/637

src/Concerns/MakesAssertions.php
@@ -737,9 +737,10 @@ JS;
      */
     public function assertVueContains($key, $value, $componentSelector = null)
     {
-        PHPUnit::assertTrue(
-            Str::contains($this->vueAttribute($componentSelector, $key), $value)
-        );
+        $attribute = $this->vueAttribute($componentSelector, $key);
+
+        PHPUnit::assertIsArray($attribute, "The attribute for key [$key] is not an array.");
+        PHPUnit::assertContains($value, $attribute);
 
         return $this;
     }
@@ -755,9 +756,10 @@ JS;
      */
     public function assertVueDoesNotContain($key, $value, $componentSelector = null)
     {
-        PHPUnit::assertFalse(
-            Str::contains($this->vueAttribute($componentSelector, $key), $value)
-        );
+        $attribute = $this->vueAttribute($componentSelector, $key);
+
+        PHPUnit::assertIsArray($attribute, "The attribute for key [$key] is not an array.");
+        PHPUnit::assertNotContains($value, $attribute);
 
         return $this;
     }

tests/MakesAssertionsTest.php
@@ -198,4 +198,48 @@ class MakesAssertionsTest extends TestCase
             );
         }
     }
+
+    public function test_assert_vue_contains()
+    {
+        $driver = m::mock(stdClass::class);
+        $driver->shouldReceive('executeScript')->andReturn(['john']);
+
+        $resolver = m::mock(stdClass::class);
+        $resolver->shouldReceive('format')->with('@vue-component')->andReturn('body foo');
+
+        $browser = new Browser($driver, $resolver);
+
+        $browser->assertVueContains('users', 'john', '@vue-component');
+
+        try {
+            $browser->assertVueDoesNotContain('users', 'john', '@vue-component');
+            $this->fail();
+        } catch (ExpectationFailedException $e) {
+            $this->assertStringContainsString(
+                "Failed asserting that an array does not contain 'john'.",
+                $e->getMessage()
+            );
+        }
+    }
+
+    public function test_assert_vue_contains_with_no_result()
+    {
+        $driver = m::mock(stdClass::class);
+        $driver->shouldReceive('executeScript')->andReturn(null);
+
+        $resolver = m::mock(stdClass::class);
+        $resolver->shouldReceive('format')->with('@vue-component')->andReturn('body foo');
+
+        $browser = new Browser($driver, $resolver);
+
+        try {
+            $browser->assertVueContains('users', 'john', '@vue-component');
+            $this->fail();
+        } catch (ExpectationFailedException $e) {
+            $this->assertStringContainsString(
+                'The attribute for key [users] is not an array.',
+                $e->getMessage()
+            );
+        }
+    }
 }