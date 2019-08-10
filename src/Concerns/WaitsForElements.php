822ffb3ae62825f905f2217c463f5bb5fe13c397
Kai Reichart <Kai-reichart@web.de>
2019-08-10 14:40:06 +0200

    fix for test

--- src/Concerns/WaitsForElements.php
+++ src/Concerns/WaitsForElements.php
@@ -85,7 +85,7 @@ trait WaitsForElements
             return Str::contains($this->resolver->findOrFail('')->getText(), $text);
         }, $message);
     }
-    
+
     /**
      * @param string $selector
      * @param string $text
@@ -95,7 +95,7 @@ trait WaitsForElements
      */
     public function waitForTextIn($selector, $text, $seconds = null)
     {
-        $message = 'Waited %s seconds for text "'.$text.'" in selector ' . $selector;
+        $message = 'Waited %s seconds for text "'.$text.'" in selector '.$selector;
 
         return $this->waitUsing($seconds, 100, function () use ($selector, $text) {
             return $this->assertSeeIn($selector, $text);
