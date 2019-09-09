7bec1729a7add54228918f46ca1b5880045d700d
Adrienne L. Travis <adrienne@utilitarienne.com>
2019-09-08 21:50:13 -0700

    Creating a config file

---
+++ config/dusk.php
@@ -0,0 +1,16 @@
+    
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Dusk Selector Prefix
+    |--------------------------------------------------------------------------
+    |
+    | This is the prefix Dusk will use when resolving selectors prefixed with @
+    | (see https://laravel.com/docs/master/dusk#dusk-selectors)
+    |
+    */
+    'selector_prefix' => 'dusk',
+
+];
