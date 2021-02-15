00f5b31f2bfc5ec694ea5a0eb4295ffcf4b16a55
Dries Vints <driesvints@users.noreply.github.com>
2021-02-15 11:51:59 +0000

    Apply fixes from StyleCI

--- tests/Concerns/SwapsUrlGenerator.php
+++ tests/Concerns/SwapsUrlGenerator.php
@@ -12,10 +12,10 @@ trait SwapsUrlGenerator
             return new class {
                 public function route($name, $parameters = [], $absolute = true)
                 {
-                    $route = '/' . $name . '/' . implode('/', $parameters);
+                    $route = '/'.$name.'/'.implode('/', $parameters);
 
                     if ($absolute) {
-                        $route = 'http://www.google.com' . $route;
+                        $route = 'http://www.google.com'.$route;
                     }
 
                     return $route;
