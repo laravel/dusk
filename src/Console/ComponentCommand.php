49019804b0fc994452d989decaecce333a7ab2af
Tim MacDonald <hello@timacdonald.me>
2024-03-20 07:09:53 +1100

    [8.x] Make commands lazy (#1092)
    * Make commands lazy
    
    * Make commands lazy

--- src/Console/ComponentCommand.php
+++ src/Console/ComponentCommand.php
@@ -4,7 +4,9 @@ namespace Laravel\Dusk\Console;
 
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: 'dusk:component')]
 class ComponentCommand extends GeneratorCommand
 {
     /**
