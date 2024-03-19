c759f72dc795f98a21586fa9eba12d08333bc72a
Tim MacDonald <hello@timacdonald.me>
2024-03-19 14:04:34 +1100

    Make commands lazy

--- src/Console/MakeCommand.php
+++ src/Console/MakeCommand.php
@@ -4,7 +4,9 @@ namespace Laravel\Dusk\Console;
 
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: 'dusk:make')]
 class MakeCommand extends GeneratorCommand
 {
     use Concerns\InteractsWithTestingFrameworks;
--- src/Console/PageCommand.php
+++ src/Console/PageCommand.php
@@ -4,7 +4,9 @@ namespace Laravel\Dusk\Console;
 
 use Illuminate\Console\GeneratorCommand;
 use Illuminate\Support\Str;
+use Symfony\Component\Console\Attribute\AsCommand;
 
+#[AsCommand(name: 'dusk:page')]
 class PageCommand extends GeneratorCommand
 {
     /**
