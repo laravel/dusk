284f7cb43fa356cbdc2bcfa919f06d42896657e6
Pascal Baljet <pascal@pascalbaljet.nl>
2024-08-01 16:10:44 +0200

    Added `--disable-search-engine-choice-screen` argument (#1111)

--- tests/Browser/DuskTestCase.php
+++ tests/Browser/DuskTestCase.php
@@ -57,6 +57,7 @@ class DuskTestCase extends TestCase
     {
         $options = (new ChromeOptions)->addArguments(collect([
             $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
+            '--disable-search-engine-choice-screen',
         ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
             return $items->merge([
                 '--disable-gpu',
